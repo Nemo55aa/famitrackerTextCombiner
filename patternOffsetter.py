9c66f7c90b54c3dc0b0574e4428809878ea0d14f
Nemo55aa <minato.work1@gmail.com>
2025-12-29 22:14:44 +0900

    ・ORDER行を用意する処理を追加 (オフセットされた分のPATTERNが曲の再生に使用されるよう修正)

--- patternOffsetter.py
+++ patternOffsetter.py
@@ -1,4 +1,4 @@
-debug = False
+debug = True
 
 def offsetPTRNline(strLine:str, offset:int) -> str:
 	tmpNumPattern :int = 0
@@ -22,7 +22,22 @@ def main():
 	for idxNum in range(len(lines)):
 		if("PATTERN" in lines[idxNum]):
 			lines[idxNum] = offsetPTRNline(lines[idxNum], numPatternOffset)
-	
+		if("ORDER" in lines[idxNum] and (not ("ORDER" in lines[idxNum + 1]))):
+			numStr = lines[idxNum].rstrip()[-2:]
+			# ------ 追加分のORDERの行を作る ------
+			tmpOrderList = []
+			for addIdx in range(numPatternOffset - 1):
+				tmpNum = int(numStr, 16) + addIdx + 1
+				# ORDER 20 : 20 20 20 .... などをもとの文字列に対して置換をして作成する
+				lineReplaced = lines[idxNum].rstrip().replace(numStr, '{:X}'.format(tmpNum)) + "\n"
+				tmpOrderList.append(lineReplaced)
+			# --- 後述のリスト挿入処理用に位置を保存しておく ---
+			idxLastLineOfORDER = idxNum + 1
+			
+	# --- 作ったORDERの行をファイル書き込み用のリストへ挿入する ---
+	if(True == debug):				
+		print(tmpOrderList)
+		lines[idxLastLineOfORDER:idxLastLineOfORDER] = tmpOrderList
 	f = open("./out.txt", "w")
 	f.writelines(lines)
 	f.close()
