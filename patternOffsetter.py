d2eb991d59c429393f4cc74bcfce7e8b4147838e
Nemo55aa <minato.work1@gmail.com>
2025-12-31 22:22:47 +0900

    ・ｺﾏﾝﾄﾞﾗｲﾝ引数でファイルおよびオフセット数の指定ができるようにした
    (debugフラグ無効時にORDERが出力されない不具合を修正)

--- patternOffsetter.py
+++ patternOffsetter.py
@@ -1,4 +1,9 @@
-debug = True
+import sys # for commandline arg
+import os
+import tkinter.messagebox as tkmbx
+import tkinter.filedialog as tkfdlg
+
+debug = False
 
 def offsetPTRNline(strLine:str, offset:int) -> str:
 	tmpNumPattern :int = 0
@@ -14,8 +19,52 @@ def offsetPTRNline(strLine:str, offset:int) -> str:
 	return strLine
 
 def main():
-	numPatternOffset = int('11', 16)
-	f = open('./The Rabbit has Landed.txt', "r+")
+	if(0):	# 251231 to add commandline args
+		numPatternOffset = int('11', 16)
+	# (end 251231)
+
+	if(0):
+		# for future convinience 
+		tkmbx.showinfo(title="Select file", message="Select first famitracker txt file")
+		
+		fTyp = [("Famitracker Text file", "*.txt")]
+		iDir = os.path.abspath(os.path.dirname(__file__))
+		file_name = tkfdlg.askopenfilename(filetypes=fTyp, initialdir=iDir)
+		if(file_name == ''):
+			tkmbx.showerror(message="file not selected!\nleaving...")
+			return -1
+		
+	args = sys.argv
+	if(3 != len(args)):
+		print("usage:")
+		print("\tpatternOffsetter [txt that youwanna of set] [number of offset]")
+		
+		print("\tpatternOffsetter:")
+		print("\t\tthis command is for textfile exported by famitracker")
+		print("\t\tand this is useful when you wanna combine two separately composed track")
+		
+		print("\t[txt that youwanna of set]:")
+		print("\t\tenter text file's path that you wanna offset PATTERN and ORDER field")
+		
+		print("\t[number of offset]:")
+		print("\t\tenter number that you wanna offset")
+		print("\t\tNOTE: Please enter Number as Hexadecimal")
+		return 0
+	else:
+		file_name = args[1]
+		numPatternOffset = int(args[2], 16)
+	
+	if(0):	# 251231 to add commandline args
+		f = open('./The Rabbit has Landed.txt', "r+")
+	# (end 251231)
+
+	try	:
+		f = open(file_name, "r+")
+	except UnboundLocalError as uble:
+		print(uble.__str__)
+		print("unable to open file")
+		return -1
+	
 	lines = f.readlines()
 	f.close()
 
@@ -37,10 +86,19 @@ def main():
 	# --- 作ったORDERの行をファイル書き込み用のリストへ挿入する ---
 	if(True == debug):				
 		print(tmpOrderList)
-		lines[idxLastLineOfORDER:idxLastLineOfORDER] = tmpOrderList
-	f = open("./out.txt", "w")
-	f.writelines(lines)
-	f.close()
+	
+	lines[idxLastLineOfORDER:idxLastLineOfORDER] = tmpOrderList
+	
+	for i in range(255):
+		tmpPathToWrite = "./out" + str(i) + ".txt"
+		if(os.path.isfile(tmpPathToWrite)):
+			continue
+		else:
+			f = open(tmpPathToWrite, "w")
+			f.writelines(lines)
+			f.close()
+			print("change is written to" + tmpPathToWrite)
+			break
 
 if __name__ == '__main__':
     main()
